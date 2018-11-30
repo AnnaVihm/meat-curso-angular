ef4df5b043ad4c38fac8a412ae165a23871abd4c
anna vitoria <vitoria@dimensiva.online>
2018-11-29 23:23:05 -0300

    Adicionado um coreModule para compatilhas os serviços isolados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import {ROUTES} from './app.routes'
 
@@ -11,18 +10,16 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
-import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart.service';
 
-import {OrderService} from "./order/order.service";
 import { SharedModule } from './shared/shared.module';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
@@ -43,10 +40,11 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
     BrowserModule,
     HttpModule,
     SharedModule,
+    CoreModule,
     RouterModule.forRoot(ROUTES),
     SharedModule
   ],
-  providers: [RestaurantsService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { OrderService } from "app/order/order.service";
+
+@NgModule({
+    providers:[ShoppingCartService, RestaurantsService, OrderService]
+})
+
+//Classe utilizada para os serviços
+export class CoreModule{}
